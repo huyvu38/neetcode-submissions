29f7aefd63f91254c5fc608b0f927b5d091b051a
Huy Vu <144382505+huyvu38@users.noreply.github.com>
2026-06-20 00:53:25 +0700

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        def dfs(index, path):
+            if len(path) == k:
+                res.append(path[:])
+                return
+            for i in range(index, n+1):
+                path.append(i)
+                dfs(i+1, path)
+                path.pop()
+        dfs(1, [])
+        return res
